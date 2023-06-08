ab236f8d14cfaac2f3dbdc7497cc4ec569328fe9
Gregory <gregory_berezin@outlook.com>
2023-06-08 21:44:46 +0300

    new edits

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-
-  plugins: [
-    ['import', {libraryName: '@ant-design/react-native'}], // The difference with the Web platform is that you do not need to set the style
-  ],
-};
