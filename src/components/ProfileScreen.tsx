dae2c657ea59e60fc6c5b2c0334a29870a9e6192
Gregory <gregory_berezin@outlook.com>
2023-06-11 09:34:58 +0300

    new edits

--- src/components/ProfileScreen.tsx
+++ src/components/ProfileScreen.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {View, Text, Button} from 'react-native';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 type RootStackParamList = {
-  Profile: undefined;
+    Profile: undefined;
 };
 
 type ProfileScreenProps = NativeStackScreenProps<RootStackParamList, 'Profile'>;
 
-export const ProfileScreen: React.FC<ProfileScreenProps> = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Broker Page</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Profile')}
-      />
-    </View>
-  );
+export const ProfileScreen: React.FC<ProfileScreenProps> = ({navigation}) => {
+    return (
+        <View>
+            <Text>Broker Page</Text>
+            <Button
+                title="Go to Home"
+                onPress={() => navigation.navigate('Profile')}
+            />
+        </View>
+    );
 };
 
