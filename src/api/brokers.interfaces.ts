dc8b8e5057332b2e55b485a1def50d5f1b16a20d
Gregory <gregory_berezin@outlook.com>
2023-06-13 19:41:52 +0300

    new edits

--- src/api/brokers.interfaces.ts
+++ src/api/brokers.interfaces.ts
@@ -1,19 +1,19 @@
 export interface IBroker {
-  id: number;
-  code: string | null;
-  fullName: string;
-  isActive: boolean;
-  tariffPlans: number[];
+    id: number;
+    code: string | null;
+    fullName: string;
+    isActive: boolean;
+    tariffPlans: number[];
 }
 
 export interface IBrokerCreateParameters {
-  fullName: string;
-  code: string;
-  isActive: boolean;
+    fullName: string;
+    code: string;
+    isActive: boolean;
 }
 
 export interface IBrokerUpdateParameters {
-  id?: number;
-  code?: string;
-  fullName?: string;
+    id?: number;
+    code?: string;
+    fullName?: string;
 }
