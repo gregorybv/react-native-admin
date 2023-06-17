be1cffb73829e5c7d8cfbfb919270181ad028e94
Gregory <gregory_berezin@outlook.com>
2023-06-17 21:43:57 +0300

    new edits

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,11 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import React, {useEffect} from 'react';
-import {login, userManager} from "./api/userManager";
-import {CallbackPage} from "./pages/CallbackPage/CallbackPage";
-import {HomePage} from "./pages/HomePage/HomePage";
-import {BrokersPage} from "./pages/BrokersPage/BrokersPage";
-import {BrokerPage} from "./pages/BrokerPage/BrokerPage";
-import {PageLayout} from "./pages/PageLayout/PageLayout";
-
-const Stack = createStackNavigator();
+import {createBrowserRouter, RouteObject} from 'react-router-dom';
+import {CallbackPage} from './Callback';
+import {login, userManager} from '@/api/userManager';
+import React from 'react';
+import {HomePage} from '@/pages/HomePage';
+import {BrokersPage} from "@/pages/BrokersPage";
+import {BrokerPage} from '@/pages/BrokerPage';
+import {PageLayout} from '@/pages/PageLayout';
 
 const authLoader = async () => {
     const user = await userManager.getUser();
@@ -20,54 +17,37 @@ const authLoader = async () => {
     return null;
 };
 
-const protectedRoute = (route) => {
+const protectedRoute = (route: RouteObject): RouteObject => {
+
     return {
         ...route,
         loader: authLoader,
     };
 };
 
-const router = () => {
-    useEffect(() => {
-        authLoader();
-    }, []);
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen
-                    name="Main"
-                    component={PageLayout}
-                    options={{headerShown: false}}
-                >
-                    {() => (
-                        <>
-                            <Stack.Screen
-                                name="Home"
-                                component={HomePage}
-                                options={{headerShown: false}}
-                            />
-                            <Stack.Screen
-                                name="Brokers"
-                                component={BrokersPage}
-                                options={{headerShown: false}}
-                            />
-                            <Stack.Screen
-                                name="Broker"
-                                component={BrokerPage}
-                                options={{headerShown: false}}
-                            />
-                        </>
-                    )}
-                </Stack.Screen>
-                <Stack.Screen
-                    name="Callback"
-                    component={CallbackPage}
-                    options={{headerShown: false}}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <PageLayout/>,
+        children: [
+            protectedRoute({
+                path: '',
+                element: <HomePage/>,
+            }),
+            protectedRoute({
+                path: 'brokers',
+                element: <BrokersPage/>,
+            }),
+            protectedRoute({
+                path: 'brokers/:id',
+                element: <BrokerPage/>,
+            }),
+        ],
+    },
+    {
+        path: '/callback',
+        element: <CallbackPage/>,
+    },
+]);
 
 export default router;
