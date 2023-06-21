9dc83ece53d257c0b59cc251cc832244afb34ea2
Gregory <gregory_berezin@outlook.com>
2023-06-21 17:21:01 +0300

    new edits

---
+++ src/pages/BrokerPage/BrokerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const BrokerPage = () => {
+    return (
+        <View>
+            <Text>
+                BrokerPage
+            </Text>
+        </View>
+    );
+};
+
+export default BrokerPage;
