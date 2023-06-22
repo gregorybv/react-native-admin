f2a88a9046f3bfcb51ce6121a3e89c3c667f1eb6
Gregory <gregory_berezin@outlook.com>
2023-06-22 17:55:08 +0300

    new edits

--- src/pages/BrokerPage/BrokerPage.tsx
+++ src/pages/BrokerPage/BrokerPage.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Text, View, Image, StyleSheet, ImageBackground} from "react-native";
+
+const image = {uri: 'https://legacy.reactjs.org/logo-og.png'};
 
 const BrokerPage = () => {
     return (
-        <View>
-            <Text>
-                BrokerPage
-            </Text>
+        <View style={styles.container}>
+            <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                <Text style={styles.text}>Здесь скоро будет какой-то текст</Text>
+            </ImageBackground>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    image: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    text: {
+        color: 'white',
+        fontSize: 42,
+        lineHeight: 84,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        backgroundColor: '#000000c0',
+    },
+});
 
 export default BrokerPage;
