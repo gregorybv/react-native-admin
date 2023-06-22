5e232bb1880d9f2f0962178c416728fa845fc45a
Gregory <gregory_berezin@outlook.com>
2023-06-22 17:55:17 +0300

    new edits

--- src/pages/BrokersPage/BrokersPage.tsx
+++ src/pages/BrokersPage/BrokersPage.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Text, View, TouchableOpacity, Button} from 'react-native';
+import {NavigationProp, RouteProp} from '@react-navigation/native';
+import {RootStackParamList} from "../../types";
 
-const BrokersPage = () => {
+type BrokersPageRouteProp = RouteProp<RootStackParamList, 'Страница брокеров'>;
+type BrokersPageNavigationProp = NavigationProp<RootStackParamList, 'Страница брокеров'>;
+
+type BrokersPageProps = {
+    route: BrokersPageRouteProp;
+    navigation: BrokersPageNavigationProp;
+};
+const BrokersPage: React.FC<BrokersPageProps> = ({route, navigation}) => {
+    const handleButtonPress = () => {
+        navigation.navigate('Страница брокера');
+    };
+
     return (
         <View>
-            <Text>
-                BrokersPage
-            </Text>
+            {/*<Text>Домашняя страница</Text>*/}
+            <TouchableOpacity onPress={handleButtonPress}>
+                <Button onPress={handleButtonPress} title={'Перейти на страницу брокера'}></Button>
+            </TouchableOpacity>
         </View>
     );
 };
 
-export default BrokersPage;
+export default BrokersPage
+
+
+
