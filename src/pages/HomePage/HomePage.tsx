f140c38262b907726fe5dc1456908594f3dcc172
Gregory <gregory_berezin@outlook.com>
2023-06-23 11:09:26 +0300

    add new image to homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import {View, TouchableOpacity, Button} from 'react-native';
+import {View, TouchableOpacity, Image} from 'react-native';
 import {NavigationProp, RouteProp} from '@react-navigation/native';
 import {RootStackParamList} from "../../types";
-import {Icon, IconComponentProvider} from '@react-native-material/core'
-import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+
 
 type HomePageRouteProp = RouteProp<RootStackParamList, 'Домашняя страница'>;
 type HomePageNavigationProp = NavigationProp<RootStackParamList, 'Домашняя страница'>;
@@ -20,13 +19,11 @@ const HomePage: React.FC<HomePageProps> = ({navigation}) => {
 
     return (
         <View>
-            {/*<Text>Домашняя страница</Text>*/}
             <TouchableOpacity onPress={handleButtonPress}>
-                <Button onPress={handleButtonPress} title={'Перейти на страницу брокеров'}></Button>
-                <IconComponentProvider IconComponent={MaterialCommunityIcons}>
-                    <Icon name="robot" style={{marginLeft: '12%', marginTop: '15%'}} size={290} color="grey"/>
-                </IconComponentProvider>
+                <Image style={{marginLeft: '27%', marginTop: '45%', width: 170}}
+                       source={require('../../assets/image/robot.png')}/>
             </TouchableOpacity>
+
         </View>
     );
 };
